dd288e767f0c3d2a18fcd7c8c5681fea5b378427
AstroeHxro <memrieit@gmail.com>
2025-07-21 04:39:41 -0400

    stats function imported to main

---
+++ main.py
@@ -0,0 +1,20 @@
+from stats import word_count
+
+def get_book_text(filepath):
+
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+book_contents = get_book_text('books/frankenstein.txt')
+
+def main():
+
+    
+    print(book_contents)
+
+
+
+main()
+word_count(book_contents) 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(book_contents):
+   
+    num_words = len(book_contents.split())
+    print(f'{num_words} words found in the document')
